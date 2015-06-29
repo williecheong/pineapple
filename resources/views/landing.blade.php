2496c31e80e9fbf85f5273c063ef49a60718abf6
Willie <cheongwillie@gmail.com>
2015-06-28 22:43:32 -0400

    some text for sales talk

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -73,19 +73,28 @@
         </section>
 
         <!-- About Section -->
-        <section class="success" id="about">
+        <section class="success" id="about" style="padding-top:18px;">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-12 text-center">
-                        <h2>The Dineapple Mission</h2>
+                    <div class="col-md-6 col-md-offset-3 text-center">
+                        <img src="/assets/img/pineapple2.png" class="img-responsive">
+                        <h2>About the Kitchen</h2>
                     </div>
                 </div>
-                <div class="row">
+                <div class="row" style="text-align:justify;">
                     <div class="col-md-5 col-md-offset-1">
-                        <p>Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional LESS stylesheets for easy customization.</p>
+                        <p>
+                            Dineapple brings quality, affordability, and variety food to the people.
+                            Everyday, a new item is introduced on the website. 
+                            Orders placed on the day of are delivered at dinner time the next day.
+                        </p>
                     </div>
                     <div class="col-md-5">
-                        <p>Whether you're a student looking to showcase your work, a professional looking to attract clients, or a graphic artist looking to share your projects, this template is the perfect starting point!</p>
+                        <p>
+                            We're tired of going out to eat burgers and nasty chinese food. 
+                            But more importantly, we make good food.
+                            So why not have a {!! strtolower($title) !!} for dinner tomorrow.
+                        </p>
                     </div>
                     <div class="col-md-8 col-md-offset-2 text-center">
                         <a href="/order" class="btn btn-lg btn-default">
@@ -103,8 +112,11 @@
                 <div class="container">
                     <div class="row">
                         <div class="footer-col col-md-4">
-                            <h3>About {!! APP_NAME !!}</h3>
-                            <p>Striving to serve something new and delicious everyday.</p>
+                            <h3>{!! APP_NAME !!}</h3>
+                            <p>
+                                Serving something new <br>
+                                and delicious everyday
+                            </p>
                         </div>
                         <div class="footer-col col-md-4">
                             <h3>Contact Us</h3>
@@ -130,8 +142,8 @@
                         <div class="footer-col col-md-4">
                             <h3>Location</h3>
                             <p>
-                                Restaurant coming soon<br>
-                                Stay tuned!
+                                Delivers to anywhere in <br>
+                                Kitchener/Waterloo, ON
                             </p>
                         </div>
                     </div>
